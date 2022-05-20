dd3b94a1db0f2a3e9c46ca9a97a1cfdde37f50e8
Biune <72959444+Biune@users.noreply.github.com>
2022-05-20 18:41:19 -0500

    Update trackmania.py

--- trackmania/trackmania.py
+++ trackmania/trackmania.py
@@ -9,6 +9,7 @@ import discord
 from redbot.core import commands
 from redbot.core.bot import Red
 from redbot.core.config import Config
+from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 
 RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
 
@@ -52,7 +53,6 @@ class Trackmania(commands.Cog):
     @commands.group()
     async def trackmania(self, ctx):
         """Group for Trackmania track info."""
-
 
     @trackmania.command(name="trackinfo")
     @commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
@@ -133,3 +133,83 @@ class Trackmania(commands.Cog):
 
             await message.delete()
             await ctx.send(embed=embed)
+
+    @trackmania.command(name="randomtrack")
+    @commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
+    async def randomtrack(self, ctx, number: int):
+        """Grab random Trackmania.Exchange's tracks."""
+        if number > 25:
+            await ctx.send("Don't crash the bot please.")
+        elif number < 1:
+            await ctx.send("https://www.youtube.com/watch?v=xxhNCY21-xs")
+        else:
+            message = await ctx.send('While waiting please listen to this amazing elevator music: https://www.youtube.com/watch?v=s-UFPhz2nZ0')
+            embeds = []
+
+            async def random_track():
+                random_url = await self.req('https://trackmania.exchange/mapsearch2/search?random=1', get_or_url="url")
+
+                track_id = random_url.partition('/maps/')[2]
+                
+                url = 'https://trackmania.exchange/maps/' + track_id
+                
+                track_exc_request_url = 'https://trackmania.exchange/api/maps/get_map_info/multi/' + track_id
+                
+                map_info = await self.req(track_exc_request_url, get_or_url="get")
+                map_info = map_info[0]
+
+                if map_info == '[]':
+                    await message.delete()
+                    await ctx.send("Bruv the thing that you put in the thingy doesn't have a track.")
+                else:
+                    author_name = re.findall('(?<="Username":").*(?=","GbxMapName")', map_info)
+                    author_time = re.findall('(?<="AuthorTime":).*(?=,"ParserVersion")', map_info)
+
+                    author_time = int(author_time[0])
+                    author_time = author_time / 1000
+                    author_time = str(author_time)
+
+                    name = re.findall('(?<="Name":").*(?=","Tags")', map_info)
+                    length = re.findall('(?<="LengthName":").*(?=","DifficultyName")', map_info)
+                    difficulty = re.findall('(?<="DifficultyName":").*(?=","Laps")', map_info)
+                    rating = re.findall('(?<="RatingVoteAverage":).*(?=,"HasScreenshot")', map_info)
+                    track_photo = str('https://trackmania.exchange/tracks/screenshot/normal/' + track_id)
+                    track_desc = str("Rating: " + rating[0])
+
+                    track_uid = re.findall('(?<="TrackUID":").*(?=","Mood":)', map_info)
+
+                    track_io_request_url = 'https://trackmania.io/api/leaderboard/map/' + track_uid[0] + '?offset=0&length=' + '1'
+                    wr_info = await self.req(track_io_request_url, get_or_url="get")
+                    wr_info = wr_info[0]
+
+                    record_names = []
+                    record_times = []
+
+                    async def findrecord(record_num):
+                        name = re.findall('(?<={"player":{"name":").*?(?=","tag"|","id":")', wr_info)
+                        name = name[record_num]
+                        record_names.append(name)
+
+                        time = re.findall('(?<="time":).*?(?=,"filename")', wr_info)
+                        time = int(time[record_num])
+                        time = time / 1000
+                        record_times.append(time)
+
+                    await findrecord(0)
+                    wr_time = '``' + record_names[0] + '`` set a time of ``' + str(record_times[0]) + '``'
+
+                    embed=discord.Embed(title=name[0], url=url, description=track_desc)
+                    embed.add_field(name="Author's Username", value=author_name[0], inline=True)
+                    embed.add_field(name="Author's Time", value=author_time, inline=True)
+                    embed.add_field(name="WR Time", value=wr_time, inline=True)
+                    embed.add_field(name="Track Length", value=length[0], inline=True)
+                    embed.add_field(name="Track's Difficulty", value=difficulty[0], inline=True)
+                    embed.add_field(name="Track's Rating", value=rating[0], inline=True)
+                    embed.set_thumbnail(url=track_photo)
+                    embeds.append(embed)
+
+            for x in range(1, number):
+                await random_track()
+
+            await message.delete()
+            await menu(ctx, embeds, DEFAULT_CONTROLS)
