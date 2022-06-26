743c9cae44e14a51774a4edb9acf499ed1a164be
Biune <contact@is-a.win>
2022-06-26 13:35:58 -0500

    test

--- streaming/__init__.py
+++ streaming/__init__.py
@@ -1,13 +1,4 @@
-import json
-from pathlib import Path
-
-from redbot.core.bot import Red
-
 from .streaming import Streaming
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
-
-async def setup(bot: Red) -> None:
-    bot.add_cog(Streaming(bot))
+async def setup(bot) -> None:
+    await bot.add_cog(Streaming(bot))
