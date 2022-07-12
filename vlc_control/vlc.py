2736f337cf068aa9d22540fccd3341d269f1fbbf
Biune <you@example.com>
2022-07-12 14:44:33 -0500

    add vlc

--- vlc_control/vlc.py
+++ vlc_control/vlc.py
@@ -102,5 +102,5 @@ class VLC(commands.Cog):
         number = int(message.content)
         item_id = items[number]['id']
         item_id = item_id.replace('plid', '')
-
+        print(f'{url}/requests/status.xml?command=pl_play&id={item_id}')
         await self.session.get(f'{url}/requests/status.xml?command=pl_play&id={item_id}', auth=aiohttp.BasicAuth('', password=password))
