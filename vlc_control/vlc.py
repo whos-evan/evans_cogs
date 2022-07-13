e7b49177eabd9085c8fccb07c8901e934e28b7ad
Biune <you@example.com>
2022-07-12 20:33:12 -0500

    test

--- vlc_control/vlc.py
+++ vlc_control/vlc.py
@@ -29,7 +29,8 @@ class VLC(commands.Cog):
 
         default_guild = {
             "url": "http://localhost:8080",
-            "password": "test"
+            "password": "test",
+            "modonly": False
         }
 
         self.config.register_guild(**default_guild)
@@ -74,52 +75,70 @@ class VLC(commands.Cog):
             await self.config.custom("VLCGroup", ctx.guild.id).password.set(password)
             await ctx.send("Password set to {}".format(password))
 
+    @vlc.command(name="modonly")
+    @commands.admin_or_permissions(manage_guild=True)
+    async def modonly(self, ctx):
+        """
+        Set the modonly setting.
+        """
+        settings = await self.config.custom("VLCGroup", ctx.guild.id).modonly()
+        if settings is True:
+            await self.config.custom("VLCGroup", ctx.guild.id).password.set(False)
+            await ctx.send("Modonly has been disabled.")
+        else:
+            await self.config.custom("VLCGroup", ctx.guild.id).password.set(True)
+            await ctx.send("Modonly has been enabled.")
+
     @vlc.command(name="search")
     @commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
     async def search(self, ctx, *, search: str):
         """Search for stuff in VLC"""
-        url = await self.config.custom("VLCGroup", ctx.guild.id).url()
-        password = await self.config.custom("VLCGroup", ctx.guild.id).password()
-        full_search = await self.session.get(f'{url}/requests/playlist_jstree.xml', auth=aiohttp.BasicAuth('', password=password))
-        full_search = await full_search.text()
-        root = ET.fromstring(full_search)
-
-        searched_items = []
-        
-        for item in root.findall('.//item/*'):
-            if search.lower() in str(item.attrib).lower():
-                searched_items.append(item.attrib['id'].replace('plid_', '') + ' - ' + item.attrib['name'])
-                searched = item.findall('.//item')
-                for item in searched:
-                    searched_items.append(item.attrib['id'].replace('plid_', '') + ' - ' + item.attrib['name'])
-
-        searched_items = list( dict.fromkeys(searched_items) )
-        searched_list = '\n'.join(searched_items)
-
-        def check(m: discord.Message):  # m = discord.Message.
-            return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id 
-        
-        if len(searched_items) == 0:
-            await ctx.send("No results found.")
+        modonly = await self.config.custom("VLCGroup", ctx.guild.id).modonly()
+        if modonly and not ctx.author.guild_permissions.administrator:
+            return
         else:
-            await ctx.send(f"{len(searched_items)} results found. Please type the number next to the item you wish to play.\n{searched_list}")
-            try:
-                message = await self.bot.wait_for('message', check=check, timeout=60.0)
-                number = int(message.content)
-                if number > 200000:
-                    await ctx.send("Number invalid.")
-                elif number < 0:
-                    await ctx.send("Number invalid.")
-                else:
-                    for item in searched_items:
-                        if str(number) in item.split(' - ')[0]:
-                            requested = item
-                    request = await self.session.get(f'{url}/requests/status.xml?command=pl_play&id={number}', auth=aiohttp.BasicAuth('', password=password))
-                    if request.status == 200:
-                        await ctx.send(f"Playing: {requested}")
+            url = await self.config.custom("VLCGroup", ctx.guild.id).url()
+            password = await self.config.custom("VLCGroup", ctx.guild.id).password()
+            full_search = await self.session.get(f'{url}/requests/playlist_jstree.xml', auth=aiohttp.BasicAuth('', password=password))
+            full_search = await full_search.text()
+            root = ET.fromstring(full_search)
+
+            searched_items = []
+            
+            for item in root.findall('.//item/*'):
+                if search.lower() in str(item.attrib).lower():
+                    searched_items.append(item.attrib['id'].replace('plid_', '') + ' - ' + item.attrib['name'])
+                    searched = item.findall('.//item')
+                    for item in searched:
+                        searched_items.append(item.attrib['id'].replace('plid_', '') + ' - ' + item.attrib['name'])
+
+            searched_items = list( dict.fromkeys(searched_items) )
+            searched_list = '\n'.join(searched_items)
+
+            def check(m: discord.Message):  # m = discord.Message.
+                return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id 
+            
+            if len(searched_items) == 0:
+                await ctx.send("No results found.")
+            else:
+                await ctx.send(f"{len(searched_items)} results found. Please type the number next to the item you wish to play.\n{searched_list}")
+                try:
+                    message = await self.bot.wait_for('message', check=check, timeout=60.0)
+                    number = int(message.content)
+                    if number > 200000:
+                        await ctx.send("Number invalid.")
+                    elif number < 0:
+                        await ctx.send("Number invalid.")
                     else:
-                        await ctx.send("Error while searching for that item number. Please try again.")
-            except asyncio.TimeoutError:
-                await ctx.send("Timed out.")
-            except:
-                await ctx.send("Error while searching for that item number. Please try again.")
+                        for item in searched_items:
+                            if str(number) in item.split(' - ')[0]:
+                                requested = item
+                        request = await self.session.get(f'{url}/requests/status.xml?command=pl_play&id={number}', auth=aiohttp.BasicAuth('', password=password))
+                        if request.status == 200:
+                            await ctx.send(f"Playing: {requested}")
+                        else:
+                            await ctx.send("Error while searching for that item number. Please try again.")
+                except asyncio.TimeoutError:
+                    await ctx.send("Timed out.")
+                except:
+                    await ctx.send("Error while searching for that item number. Please try again.")
